73d125b523f5dc0d15dcafedf07aa830fd93e736
subhranshudev <subhranshusekhar2509@gmail.com>
2024-07-14 11:32:14 +0000

    conversion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,29 @@
+ let score = true
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+ let ValueInNumber = Number(score)
+// console.log(typeof ValueInNumber)
+// console.log(ValueInNumber)
+
+
+// "33" =>33
+// "33abc" => NaN
+// true => 1; false=> 0
+
+let IsLoggedIn = "subhranshu"
+
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+
+// 1=>true; 0=>false
+//"" =>false
+//"subhranshu"=>true
+
+let SomeNumber = 33
+
+let StringNumber = String(SomeNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
