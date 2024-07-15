e6d74c626744f4a829226a5b211c180149413021
subhranshudev <subhranshusekhar2509@gmail.com>
2024-07-15 03:00:30 +0000

    Conversion and Operation done

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -25,5 +25,44 @@ let booleanIsLoggedIn = Boolean(IsLoggedIn)
 let SomeNumber = 33
 
 let StringNumber = String(SomeNumber)
-console.log(StringNumber)
-console.log(typeof StringNumber)
+//console.log(StringNumber)
+//console.log(typeof StringNumber);
+
+//************ OPERATION **************//
+
+let value = 3
+let negvalue = -3
+//console.log(negvalue)  
+
+// console.log( 2+2 );
+// console.log( 2-2 );
+// console.log( 2*2 );
+// console.log( 2**3 );
+// console.log( 2/2 );
+// console.log( 2%3 );
+
+let str1 = "hello"
+let str2 = " subhranshu"
+let str3 = str1 + str2
+//console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 +"2")
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1 =num2 =num3 =2+2
+
+let gameCounter = 100
+gameCounter++;
+//console.log(gameCounter);
+
+let x = 3;
+y = x++
+z = ++x
+//console.log([x],[y],[z])
