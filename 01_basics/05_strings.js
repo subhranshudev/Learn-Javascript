43871d817e5a28f90fd8c0abb9f417aaf0bb09df
subhranshudev <subhranshusekhar2509@gmail.com>
2024-07-15 14:46:10 +0000

    strings learn again

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,6 @@
+const name = "subhranshu"
+const repoCount = 1
+
+// console.log( name + repoCount + " value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // modern String
