4be03e434a3117cd485b06db09471faf131f496f
subhranshudev <subhranshusekhar2509@gmail.com>
2024-07-16 07:10:42 +0000

    string done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,4 +3,30 @@ const repoCount = 1
 
 // console.log( name + repoCount + " value");
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // modern String
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // modern String  ---  string interpolation
+
+const gameName = new String("subhranshu-sekhar-com")
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('b'));
+
+const newString = gameName.substring(0 , 4)
+//console.log(newString);
+
+const anotherString = gameName.slice(-9, 4)
+//console.log(anotherString);
+
+const newStringOne = "    subhranshu    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url  = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20', '-'));
+console.log(url.includes('hitesh'));
+console.log(url.includes('sundar'));
+console.log(gameName.split('-'));
