175151c127510763a09206479b915a6489c97bb1
subhranshudev <subhranshusekhar2509@gmail.com>
2024-07-18 06:00:38 +0000

    function basic done

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -41,4 +41,37 @@ function loginUserMessage(username) {
    return `${username} just logged in`
 }
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+// console.log(loginUserMessage("sekhar"))
+
+
+function calculateCartPrice(val1, val2, ...num1){                // ' ... ' --->rest
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500,2000))
+
+const user ={
+    username: "subhranshu",
+    price: 399
+}
+
+function handelObject(anyObject){
+    console.log(`User name is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handelObject(user)
+handelObject({
+    username: "sam",
+    price:199
+})
+
+const myNewArray =[200, 400,100,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray))
+
+console.log(returnSecondValue([200, 400, 500, 1000]))
