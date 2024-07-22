6d33eac77350b1b5903bdd3cd0a4baded281153d
subhranshusekhar <subhranshusekhar2509@gmail.com>
2024-07-22 17:47:11 +0000

    forEach and filter done

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -36,4 +36,9 @@ const books = [
     { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
   ];
 
-  let userBooks = books.filter( (bk) => bk. )
+  let userBooks = books.filter( (bk) => bk.genre ==='History' )
+
+  userBooks = books.filter( (bk) => {return bk.publish >= 1995 && bk.genre === 'History'})
+
+
+console.log(userBooks);
