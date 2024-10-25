f23487aef75561a0516e4d118d384e440e4a649f
subhranshusekhar <subhranshusekhar2509@gmail.com>
2024-10-25 07:18:04 +0000

    JSON  done

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -57,7 +57,7 @@ const users = [
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
-
+//************  de-structure  **********************/
 const course = {
     courseName: "js in hindi",
     price: "999",
@@ -67,8 +67,24 @@ const course = {
 // course.courseInstructor
 
 // const {courseInstructor: instructor} = course
-// console.log(instructor);
 
+// console.log(courseInstructor);   
+
+// console.log(instructor);        // we can also rename the keys when de-dtructuring
+
+
+//************ JSON  ****************/
+
+//  JSON, which stands for JavaScript Object Notation, is a lightweight data interchange format that's easy 
+//  for humans to read and write and easy for machines to parse and generate. It’s commonly used to transmit 
+//  data between a server and a web application.
+
+// Key Characteristics of JSON:
+//    Text-Based Format: JSON is a text format that can be easily read and written by humans.
+//    Data Structure: It represents data as key-value pairs, similar to JavaScript objects.
+
+// Syntax:  
+//     Objects: Enclosed in curly braces {}. Contains key-value pairs. For example:
 
 // {
 //     "name": "subhranshu",
@@ -76,8 +92,30 @@ const course = {
 //     "price": "free"
 // }
 
+ // Sometimes the data also comes in form of Array
 [
     {},
     {},
     {}
 ]
+
+// Using JSON in JavaScript:
+//   1. Parsing JSON:
+//    To convert a JSON string into a JavaScript object, we can use JSON.parse():
+
+        const jsonString = '{"name": "Alice", "age": 25}';
+        const userObject = JSON.parse(jsonString);
+    //  console.log(userObject.name); // Outputs: Alice
+
+
+// 2. Stringifying Objects:
+// To convert a JavaScript object into a JSON string, we can use JSON.stringify():
+
+        const user = {
+            name: "Alice",
+            age: 25
+        };
+        const JsonString = JSON.stringify(user);
+//      console.log(JsonString); // Outputs: {"name":"Alice","age":25}
+  
+// JSON is commonly used for sending data between a client and a server in web applications (e.g., through APIs).
